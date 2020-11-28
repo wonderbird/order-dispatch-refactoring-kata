49800a4e0704314dbadfe3de3587a0abbf0019a6
Gregor Riegler <gregor.riegler@ebcont.com>
2020-11-28 21:49:11 +0100

    r un-@Data Order

--- java-lombok/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
+++ java-lombok/src/main/java/it/gabrieletondi/telldontaskkata/domain/Order.java
@@ -9,7 +9,6 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Data
 public class Order {
     private BigDecimal total;
     private String currency;
@@ -17,6 +16,54 @@ public class Order {
     private BigDecimal tax;
     private OrderStatus status;
     private int id;
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public void approveOrReject(OrderApprovalRequest request) {
         if (getStatus().equals(OrderStatus.SHIPPED)) {
