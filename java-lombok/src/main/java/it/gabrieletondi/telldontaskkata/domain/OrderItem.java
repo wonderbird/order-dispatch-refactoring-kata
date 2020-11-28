d6d44ddc80c0a6f3baf753efbd6e3aab78e66f72
Gregor Riegler <gregor.riegler@ebcont.com>
2020-11-28 23:09:40 +0100

    R inline OrderItem setters

--- java-lombok/src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ java-lombok/src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -1,26 +1,39 @@
 package it.gabrieletondi.telldontaskkata.domain;
 
-import lombok.Data;
-
 import java.math.BigDecimal;
 
 import static java.math.RoundingMode.HALF_UP;
 
-@Data
 public class OrderItem {
     private Product product;
     private int quantity;
     private BigDecimal taxedAmount;
     private BigDecimal tax;
 
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getTaxedAmount() {
+        return taxedAmount;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
     public static OrderItem create(Product product, int quantity) {
         final OrderItem orderItem = new OrderItem();
-        orderItem.setProduct(product);
-        orderItem.setQuantity(quantity);
-        orderItem.setTax(product.tax().multiply(BigDecimal.valueOf(quantity)));
-        orderItem.setTaxedAmount(product.taxedPrice()
+        orderItem.product = product;
+        orderItem.quantity = quantity;
+        orderItem.tax = product.tax().multiply(BigDecimal.valueOf(quantity)).setScale(2, HALF_UP);
+        orderItem.taxedAmount = product.taxedPrice()
             .multiply(BigDecimal.valueOf(quantity))
-            .setScale(2, HALF_UP));
+            .setScale(2, HALF_UP);
         return orderItem;
     }
 }
